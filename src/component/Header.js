75078d8dc7146dae3d7a23afc720647c1d9d3546
Waheed Khan <wahid.khan74@gmail.com>
2021-06-11 04:51:02 +0000

    setup

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Header extends React.Component {
+
+    render() {
+        return (
+            <h2>Blogger App</h2>
+        )
+    }
+}
+
+export default Header;
